ab764d85002b749ebd6b7e8c9a2ca21eee8a636d
Milo Fultz <milopfultz@gmail.com>
2021-06-08 18:33:13 -0700

    Refactor Colors to C

--- menu.py
+++ menu.py
@@ -3,7 +3,8 @@ import re
 import sys
 
 import tasks
-from utilities import (Colors, clear_screen, show_help,
+from utilities import Colors as C
+from utilities import (clear_screen, show_help,
                        load_data, save_data, format_tasks_to_plaintext,
                        task_number_input, task_name_input, task_time_input,
                        spend_time_on_task, convert_time_spent_to_seconds,
@@ -21,7 +22,7 @@ def main_menu(active_tasks: list[dict]):
                   if re.match(r'[A-Za-z]\d+', command)
                   else None)
         if number is not None and number >= len(active_tasks):
-            print(Colors.RED + "No such task.\n" + Colors.NORMAL)
+            print(C.RED + "No such task.\n" + C.NORMAL)
             continue
 
         if command == '':
@@ -31,7 +32,7 @@ def main_menu(active_tasks: list[dict]):
             try:
                 task = active_tasks[number]
             except IndexError:
-                print(Colors.RED + "No such task.\n" + Colors.NORMAL)
+                print(C.RED + "No such task.\n" + C.NORMAL)
                 continue                
             time_spent_in_seconds = spend_time_on_task(task['name'])
             prev_time_spent_in_seconds = convert_time_spent_to_seconds(task['time_spent'])
@@ -42,35 +43,35 @@ def main_menu(active_tasks: list[dict]):
                         formatted_time_spent,
                         number)
             clear_screen()
-            print(Colors.PURPLE + 'Elapsed time added.' + Colors.NORMAL)
+            print(C.PURPLE + 'Elapsed time added.' + C.NORMAL)
         elif 'a' in command[0]:
             task_name = task_name_input()
             active_tasks = tasks.add(active_tasks, task_name, '0:00', number)
             clear_screen()
-            print(Colors.PURPLE + 'Task added.' + Colors.NORMAL)
+            print(C.PURPLE + 'Task added.' + C.NORMAL)
         elif 'c' in command[0]:
             if number is None:
                 number = task_number_input(len(active_tasks))
             active_tasks = tasks.set_completion(active_tasks, number)
             clear_screen()
-            print(Colors.PURPLE + 'Task updated.' + Colors.NORMAL)
+            print(C.PURPLE + 'Task updated.' + C.NORMAL)
         elif len(command) == 2 and command == 'dd':
             active_tasks = []
             clear_screen()
-            print(Colors.PURPLE + 'Tasks deleted.' + Colors.NORMAL)
+            print(C.PURPLE + 'Tasks deleted.' + C.NORMAL)
         elif 'd' in command[0]:
             if number is None:
                 number = task_number_input(len(active_tasks))
             active_tasks = tasks.delete(active_tasks, number)
             clear_screen()
-            print(Colors.PURPLE + 'Task deleted.' + Colors.NORMAL)
+            print(C.PURPLE + 'Task deleted.' + C.NORMAL)
         elif 'e' in command[0]:
             if number is None:
                 print_all_tasks(active_tasks)
                 number = task_number_input(len(active_tasks))
                 clear_screen()
             task = active_tasks[number]
-            print('\n' + Colors.BLUE + "Original Task:" + Colors.NORMAL)
+            print('\n' + C.BLUE + "Original Task:" + C.NORMAL)
             print(f"\n{task['name']} ({task['time_spent']})\n")
             updated_task_name = task_name_input(task['name'])
             updated_time_spent = task_time_input(task['time_spent'])
@@ -79,7 +80,7 @@ def main_menu(active_tasks: list[dict]):
                                        updated_time_spent,
                                        number)
             clear_screen()
-            print(Colors.PURPLE + 'Task updated.' + Colors.NORMAL)
+            print(C.PURPLE + 'Task updated.' + C.NORMAL)
         elif 'h' in command[0]:
             show_help()
         elif 'm' in command[0]:
@@ -89,19 +90,19 @@ def main_menu(active_tasks: list[dict]):
             to_number = int(input(f'Move task {number} to where? '))
             active_tasks = tasks.move(active_tasks, number, to_number)
             clear_screen()
-            print(Colors.PURPLE + 'Tasks updated.' + Colors.NORMAL)
+            print(C.PURPLE + 'Tasks updated.' + C.NORMAL)
         elif 'q' in command[0]:
             sys.exit()
         elif 'r' in command[0]:
             active_tasks = tasks.reduce(active_tasks)
-            print(Colors.PURPLE + 'Tasks reduced.' + Colors.NORMAL)
+            print(C.PURPLE + 'Tasks reduced.' + C.NORMAL)
         elif 's' in command[0]:
             print('Starting new task list...\n')
             active_tasks = start_new_task_list()
             clear_screen()
         else:
-            print(Colors.WHITE + "Try 'help' for more information." +
-                  Colors.NORMAL)
+            print(C.WHITE + "Try 'help' for more information." +
+                  C.NORMAL)
 
         data = format_tasks_to_plaintext(active_tasks)
         save_data(data, os.getenv('TOD_FP'))
